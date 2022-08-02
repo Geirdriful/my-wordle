3307e78e332720610d49c61c7fac4404a15ca6ba
Geirdriful <jouancedric02@gmail.com>
2022-08-02 12:25:00 +0200

    test script

--- git_add_distant.sh
+++ git_add_distant.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-read -p "Commit description: " desc
-git add . && \
-git commit -m "$desc"
+# read -p "Commit description: " desc
+# git add . && \
+# git commit -m "$desc"
 git push -u origin main  
