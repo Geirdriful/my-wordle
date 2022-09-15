04dd6cdf615e8a0a20edbda1647a4119f9fc192c
Geirdriful <cedric@DESKTOP-LMPVE43.localdomain>
2022-09-15 15:03:39 +0200

    add mock class "MockResponse" to mock "is_valid" function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,14 +4,17 @@ import sys
 sys.path.insert(1, '/home/cedric/my_wordle')
 import service_api
 import utils
-from unittest.mock import MagicMock
+# from unittest.mock import MagicMock
+
+from unittest import mock
 
 class TestUtilsBasicCases(unittest.TestCase):
+	def is_valid_mock(*args, **kwargs):
+		class MockResponse:
+			def __init__(self, word, status):
+				self.wotd = word
 
-	def is_valid_mock(self, value):
-		if value == 'bonjour':
-			return True
-		return False
+		return MockResponse({"mot": 'word'}, 200)
 
 	# Constructor
 	def __init__(self, *args, **kwargs):
@@ -20,7 +23,7 @@ class TestUtilsBasicCases(unittest.TestCase):
 		self.wotd = 'VENDREDI'
 
 		self.mock_service = service_api.ServiceAPI()
-		self.mock_service.is_valid = MagicMock(side_effect = self.is_valid_mock)
+		self.mock_service.is_valid = mock.MagicMock(side_effect = self.is_valid_mock)
 		
 	# # Execute before each methods
 	# def setUp(self):
@@ -30,7 +33,11 @@ class TestUtilsBasicCases(unittest.TestCase):
 	# 	self.wotd = ""
 
 	def test_given_an_existing_word_when_checking_word_validity_then_should_be_OK(self):
-		self.assertTrue(self.mock_service.is_valid('bonjour'))
+
+		# self.assertTrue(self.mock_service.is_valid('bonjour'))
+		# print(valid_word.wotd['mot'])
+		valid_word = self.mock_service.is_valid('bonjour')
+		self.assertEqual(self.mock_service.is_valid('bonjour').wotd, {"mot": 'word'})
 
 	def test_given_the_real_word_of_the_day_when_checking_word_of_the_day_then_should_be_OK(self):
 		
@@ -79,16 +86,16 @@ class TestUtilsErrorCases(unittest.TestCase):
 		self.assertNotEqual(l[::-1], utils.green_letters(self.wotd, self.wotd))
 
 	def test_given_a_player_word_when_checking_if_the_letters_are_in_the_wotd_but_not_at_the_good_place_should_be_KO(self):
+		
 		player_word = 'VALSE'
 		wotd = 'AIGLE'
-		print(utils.orange_letters(wotd, player_word))
-		self.assertNotEqual([0,3,4], utils.orange_letters(wotd, player_word))
+		self.assertNotEqual([1,2,4], utils.orange_letters(wotd, player_word))
 
-	def test_given_a_player_word_with_redundant_char_when_wotd_does_not_then_should_have_only_first_letter_in_orange(self):
+	def test_given_a_player_word_with_redundant_char_when_wotd_does_not_then_should_not_have_only_first_letter_in_orange(self):
+		
 		player_word = 'ELIME'
 		wotd = 'AIGLE'
-		print(utils.orange_letters(wotd, player_word))
-		self.assertNotEqual([3,4], utils.orange_letters(wotd, player_word))
+		self.assertNotEqual([0,1,2,4], utils.orange_letters(wotd, player_word))
 
 if __name__ == '__main__':
 	unittest.main()
